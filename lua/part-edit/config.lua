3459df411221f611b9690ba7edc3055897f317ae
niuiic <niuiicde@gmail.com>
2025-11-26 23:41:14 +0800

    refactor: config

---
+++ lua/part-edit/config.lua
@@ -0,0 +1,25 @@
+local Config = {}
+
+function Config:new(config)
+	local instance = {
+		_config = config,
+	}
+
+	setmetatable(instance, {
+		__index = Config,
+	})
+
+	return instance
+end
+
+function Config:set(config)
+	self._config = vim.tbl_deep_extend("force", self._config, config)
+end
+
+function Config:get()
+	return self._config
+end
+
+local default_config = {}
+
+return Config:new(default_config)
--- plugin/part-edit.lua
+++ plugin/part-edit.lua
@@ -2,6 +2,4 @@ local part_edit = require("part-edit")
 
 vim.api.nvim_create_user_command("PartEdit", function()
 	part_edit.part_edit()
-end, {
-	range = 0,
-})
+end, { range = 0 })
